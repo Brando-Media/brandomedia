f410c30f41bac37cafe477427c39779fc82ffac3
David Forson <david@brando-media.com>
2019-06-03 14:48:19 +0100

    f

--- wp-content/themes/brando/footer.php
+++ wp-content/themes/brando/footer.php
@@ -90,7 +90,7 @@
             section : '.js-section',
             sectionName:false,
             easing: "swing",
-            scrollSpeed: 650,
+            scrollSpeed: 1650,
             scrollbars: true,
             overflowscroll: true,
             interstitialSection: ".header",
