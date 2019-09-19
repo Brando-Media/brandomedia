35588d88057d8bd353cf3b374846147e63f0d046
df165 <davidbd@live.com>
2019-09-19 15:22:57 +0100

    orange font

--- wp-content/themes/brando/templates/services/content.php
+++ wp-content/themes/brando/templates/services/content.php
@@ -5,8 +5,8 @@ $sectionName = str_replace(' ', '', strtolower($title));
 ?>
 <section class="section <?php echo $bg; ?>-bg d-flex" data-section-name="<?php echo $sectionName; ?>">
     <div class="container container--small container--small align-self-center">
-        <h2 class="main-title"><?php echo $title; ?></h2>
-        <div class="row">
+        <h2 class="main-title orangefont"><?php echo $title; ?></h2>
+        <div class="row" style="margin-left:0px;">
             <div class="services-content">
                 <?php echo get_sub_field('content'); ?>
             </div>
